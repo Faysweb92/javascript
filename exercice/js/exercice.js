9ef1c6e8a03e5397e8f01457709b41f465c11806
Faysweb92 <faissal.lazaar@lepoles.org>
2023-05-24 15:54:01 +0200

    JAVA DU 2405

---
+++ exercice/js/exercice.js
@@ -0,0 +1,73 @@
+//var prenom = "mila"; //string
+
+var monNumero = 65; // numerique / number
+
+var nonNUm = "100";
+
+//var nom= izadi; // syntaxe false
+
+var booleanFalse = false; // boolean
+
+var booleanTrue = true; // boolean
+
+var varNull = null; // null
+var mitra;
+//console.log(mitra);
+//alert(mitra);
+//alert("voici mon prénom: Mitra");
+//window.alert("Super, tu es arrivé sur mon site !");
+//document.write("A la pause vous aurez des Haribos Pik !");
+//var monPrenom = prompt();
+//document.write(monPrenom);
+
+//var unBooleen = false; // -- true
+//console.log(unBooleen);
+//unBooleen = true;
+//unBooleen = "true";
+//console.log(unBooleen);
+//console.log(typeunBooleen);
+//const PAYS = "France";
+//PAYS="Italie"
+//document.write(PAYS);
+
+//var unChiffre = "12.5698";
+//console.log(unChiffre);
+//console.log(typeof unChiffre);
+
+// STRING => NUMBER
+//unChiffre = parseInt(unChiffre);
+//console.log(unChiffre);
+//console.log(typeof unChiffre);
+
+//unChiffre = parseFloat(unChiffre)
+
+// NUMBER => STRING
+//var nb_en_lettres = 258;
+//console.log(nb_en_lettres);
+//console.log(typeof nb_en_lettres);
+
+//var nb_en_lettres = nb_en_lettres.toString();
+//console.log(nb_en_lettres);
+//console.log(typeof nb_en_lettres);
+
+
+var prenom = prompt();
+document.write(prenom);
+console.log(prenom);
+alert(prenom);
+
+// Différent types de fonctions:
+// 1- une fonction avec au moin un paramètre sans valeur de retour
+//pas de retour du fils sandwich 
+// 2- une fonction avec au moin un paramètre avec valeur de retour
+//retour achete la baguette
+// 3- une fonction sans paramètre avec valeur de retour
+//cadeau
+// 4- une fonction sans paramètre sans valeur de retour La fonction alerte
+//va te changer les idée
+//comme la fonction alert
+
+
+
+
+
