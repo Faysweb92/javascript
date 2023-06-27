7d6cefaa2005001ceb3dcd7f8a7bbf76301b6964
Faysweb92 <faissal.lazaar@lepoles.org>
2023-06-27 16:34:36 +0200

    js with wb3

---
+++ JS WF3/index.js
@@ -0,0 +1,59 @@
+// var largeur = 4;
+// var longueur =2;
+// var hauteur = 3;
+// var volume = 0;
+// volume = longueur * largeur * hauteur;
+// console.log(volume);
+
+// let age = 5
+
+// if (age < 5) {
+//     console.log("bébé");
+    
+// }
+
+// if ((age >= 5) && (age <= 10)) { console.log("enfant"); }
+
+// if ((age >= 11) && (age <= 18)) { console.log("ado"); }
+// if (age > 18) {
+//     console.log("vieux");
+// }
+
+// nombre=
+
+// switch (nombre) {
+//     case 1:
+//         console.log('lundi');
+// break;
+//         case 2:
+//         console.log('mardi');
+// break;
+//         case 3:
+//         console.log('mercredi');
+// break;
+//         case 4:
+//         console.log('jeudi');
+// break;
+//         case 5:
+//         console.log('vendredi');
+// break;
+//         case 6:
+//         console.log('samedi');
+// break;
+
+//         case 7:
+//         console.log('dimanche');
+// break;
+
+//     default: console.log('ce jour nest pas bon');
+        
+// }
+
+// var age = 18;
+// var message = (age >= 18) ? "adulte" : "mineur";
+// console.log(message);
+
+
+
+
+
